d9c52cb49fa28bfabbaefa60341461704b680cf6
Matt Cooper <m.cooper44@gmail.com>
2023-06-18 20:39:30 -0400

    Added __str__ methods to dataclasses
    additionally modified how the line object is constructed by
    explicitly passing the debit or credit amount through
    as floats

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ sample = [['2022-01-17','Branch Transaction SERVICE CHARGE CAPPED MONTHLY FEE$16
 @dataclass
 class Summary:
     pass
-    
+
 
 @dataclass
 class Transaction:
@@ -27,6 +27,9 @@ class Transaction:
     tr_type: str # what was the purpose
     tr_id: str  # transaction ID (contingent)
     tr_agent: str # party to the transaction (contingent)
+
+    def __str__(self):
+        return f'{self.tr_label}, {self.tr_type}, {self.tr_id}, {self.tr_agent}'
 
 
 @dataclass
@@ -41,6 +44,10 @@ class Line:
     tr_credit: float
     tr_type: str
 
+    def __str__(self):
+        return f'{self.tr_date}, {self.tr_data}, {self.tr_debit}, {self.tr_credit}, {self.tr_type}'
+
+
 def contains_numbers(s: str) -> bool:
     '''
     helper function to parse_transaction_type
@@ -52,10 +59,10 @@ def contains_numbers(s: str) -> bool:
             return True
     return False
 
+
 def parse_transaction_type(t: str) -> tuple:
     l = t.split()
     t_n = 0
-    cat = {}
     tr_description = None
     tr_type = None
     tr_number = ''
@@ -81,32 +88,41 @@ def parse_transaction_type(t: str) -> tuple:
         if w.isnumeric():
             break
     tr_type = ' '.join([w for w in u_words if not w.isnumeric()])
-            
+
     # find a transaction number
     for k, w in enumerate(l):
         if w.isnumeric() or contains_numbers(w):
             t_n = k
             tr_number = l[k]
             break
-            
+
     # find the name of the business, recipient or depositor
     if t_n and len(l) > t_n + 1:
         tr_agent = ' '.join(l[t_n +1:])
-            
+
+    # special rule for payroll    
+    if not t_n and 'PAY' in l:
+        tr_type = f"{l[l.index('PAY')]} {l[l.index('PAY') + 1]}"
+        tr_agent = l[l.index('PAY') + 1]
+
     return tr_description, tr_type, tr_number, tr_agent
-    
-    
+
+
 def parse_file(f_path: list) -> list:
     f = []
-    for l in f_path:
-        d_or_c = 'Credit'
-        #tr = Transaction(parse_transaction_type(l[1]))
-        if l[2]:
+    for tr_line in f_path:
+        debit = 0.0
+        credit = 0.0
+        d_or_c = None
+        if tr_line[2]:
             d_or_c = 'Debit'
-        ptt = parse_transaction_type(l[1])
-        print(ptt)
-        action = Transaction(ptt[0],ptt[1],ptt[2],ptt[3])
-        f.append(Line(l[0], action, l[2], l[3], d_or_c))
+            debit = float(tr_line[2])
+        else:
+            d_or_c = 'Credit'
+            credit = float(tr_line[3])
+        ptt = parse_transaction_type(tr_line[1])
+        action = Transaction(ptt[0], ptt[1], ptt[2], ptt[3])
+        f.append(Line(tr_line[0], action, debit, credit, d_or_c))
     return f
 
 # Testing Zone
@@ -115,7 +131,5 @@ def parse_file(f_path: list) -> list:
 test = parse_file(sample)
 
 for l in test:
-    if l.tr_type == 'Debit' and l.tr_data.tr_agent:
-        print(l.tr_data.tr_agent, l.tr_data.)
-
-    
+    if l.tr_type == 'Credit' and l.tr_data.tr_agent:
+        print(l.tr_data)
