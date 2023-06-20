04affe98bcf0bbf64fd7c9e9a8f2828599f6a25e
Matt Cooper <m.cooper44@gmail.com>
2023-06-19 22:06:18 -0400

    Added Summary class + modded how dates are managed
    The Summary class will hold a dictionary keyed by month
    to hold Line objects, and a counter of debits and credits
    
    Added a __str__ and __iter__ method for the Summary Class
    the __iter__ method will yeild the list of lines by month
    and the __str__ method prints out the number of debits
    and credits the Summary holds
    
    The Summary class will be the main way to interface with the
    file and extract meaningful data from it to get higher level
    summaries of transaction types, monthly data or whatever is needed.

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import csv
 from dataclasses import dataclass, field
 from typing import List, Union
+from datetime import date
 
 
 sample = [['2022-01-17','Branch Transaction SERVICE CHARGE CAPPED MONTHLY FEE$16.95 RECORD-KEEPING N/A','16.99',''],
@@ -11,9 +12,7 @@ sample = [['2022-01-17','Branch Transaction SERVICE CHARGE CAPPED MONTHLY FEE$16
           ['2022-04-17','Electronic Funds Transfer PAY PAYROLL PAYROLL','','6000.00'],
           ['2022-05-17','Point of Sale - Interac RETAIL PURCHASE D3ADB33F0000 Subshop 00001','16.00','']]
 
-@dataclass
-class Summary:
-    pass
+
 
 
 @dataclass
@@ -38,14 +37,14 @@ class Line:
     This is an entry in the transaction record
     and is either a debit or a credit
     '''
-    tr_date: str
+    tr_date: date # isotime "2020-01-31"
     tr_data: Transaction
     tr_debit: float
     tr_credit: float
-    tr_type: str
+    tr_type: str # debit or credit
 
     def __str__(self):
-        return f'{self.tr_date}, {self.tr_data}, {self.tr_debit}, {self.tr_credit}, {self.tr_type}'
+        return f'{str(self.tr_date)}, {self.tr_data}, {self.tr_debit}, {self.tr_credit}, {self.tr_type}'
 
 
 def contains_numbers(s: str) -> bool:
@@ -100,7 +99,7 @@ def parse_transaction_type(t: str) -> tuple:
     if t_n and len(l) > t_n + 1:
         tr_agent = ' '.join(l[t_n +1:])
 
-    # special rule for payroll    
+    # special rule for payroll
     if not t_n and 'PAY' in l:
         tr_type = f"{l[l.index('PAY')]} {l[l.index('PAY') + 1]}"
         tr_agent = l[l.index('PAY') + 1]
@@ -108,8 +107,28 @@ def parse_transaction_type(t: str) -> tuple:
     return tr_description, tr_type, tr_number, tr_agent
 
 
-def parse_file(f_path: list) -> list:
-    f = []
+@dataclass
+class Summary:
+    Transaction_Months: dict = field(default_factory=dict)
+    debit_counter: int = 0
+    credit_counter: int = 0
+
+    def extract_transaction_type(self, month: int, tr_type: str = 'All') -> dict:
+        if tr_type == 'All':
+            return self.Transaction_Months
+        else:
+            return self.Transaction_Months.get(month, 'Invalid Key')
+
+    def __iter__(self):
+        for k in self.Transaction_Months.keys():
+            yield self.Transaction_Months[k]
+
+    def __str__(self):
+        return f'Debits: {self.debit_counter} Credits: {self.credit_counter}'
+
+
+
+def parse_file(f_path: list, f_summary: Summary) -> None:
     for tr_line in f_path:
         debit = 0.0
         credit = 0.0
@@ -117,19 +136,34 @@ def parse_file(f_path: list) -> list:
         if tr_line[2]:
             d_or_c = 'Debit'
             debit = float(tr_line[2])
+            f_summary.debit_counter +=1
         else:
             d_or_c = 'Credit'
             credit = float(tr_line[3])
-        ptt = parse_transaction_type(tr_line[1])
-        action = Transaction(ptt[0], ptt[1], ptt[2], ptt[3])
-        f.append(Line(tr_line[0], action, debit, credit, d_or_c))
-    return f
+            f_summary.credit_counter +=1
+
+        if not d_or_c:
+            raise 'Invalid Entry: Must contain a debit or credit - None found'
+
+        tdate = date.fromisoformat(tr_line[0])
+        tlabel, ttype, tid, tagent = parse_transaction_type(tr_line[1])
+        action = Transaction(tlabel, ttype, tid, tagent)
+
+        parsed_line = Line(tdate, action, debit, credit, d_or_c)
+
+        f_summary.Transaction_Months[tdate.month].append(parsed_line)
+    return None
 
 # Testing Zone
 
+s_obj = Summary()
+s_obj.Transaction_Months = {x : [] for x in range(1,13)}
 
-test = parse_file(sample)
+test = parse_file(sample, s_obj)
 
-for l in test:
-    if l.tr_type == 'Credit' and l.tr_data.tr_agent:
-        print(l.tr_data)
+
+for entry in s_obj:
+    if entry:
+        for trans in entry:
+            print(trans)
+
